b6224f430e2f8a6d43c08263f6d99b20591046ed
Teun Ouwehand <teun@nextpertise.nl>
2015-11-27 11:57:06 +0100

    Reshufle pieces for an extra CLI option, feature: check remote host and exit with statuscode

--- src/autorsyncbackup.py
+++ src/autorsyncbackup.py
@@ -10,12 +10,13 @@ from lib.logger import logger
 from lib.jobthread import jobThread
 
 def setupCliArguments():
+    """ Parse CLI options """
     parser = OptionParser()
     parser.add_option("-c", "--main-config", dest="mainconfig", metavar="path_to_main.yaml",
         help="set different main config file, default value = /etc/autorsyncbackup/main.yaml", 
         default="/etc/autorsyncbackup/main.yaml")
     parser.add_option("-d", "--dry-run", action="store_true", dest="dryrun", default=False,
-        help="do not invoke rsync, only perform a login attempt on remote host")
+        help="do not invoke rsync, only perform a login attempt on the remote host, when applied with -j the exit code will be set (0 for success, 1 for error)")
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False,
         help="Write logoutput also to stdout")
     parser.add_option("--version", action="store_true", dest="version", default=False,
@@ -29,7 +30,8 @@ def setupCliArguments():
 def getVersion():
     return __version__
 
-def runBackup(options):
+def runBackup(jobpath, dryrun):
+    """ Start backup run """
     exitFlag = threading.Event()
     queueLock = threading.Lock()
     workQueue = Queue.Queue(0)
@@ -38,11 +40,11 @@ def runBackup(options):
         with Pidfile(config().lockfile, logger().debug, logger().error):
             # Run director
             directorInstance = director()
-            jobs = directorInstance.getJobArray(options.job)
+            jobs = directorInstance.getJobArray(jobpath)
             
             # Start threads
             threads = []
-            if not options.dryrun:
+            if not dryrun:
                 for i in range(0, config().jobworkers):
                   thread = jobThread(i, exitFlag, queueLock, directorInstance, workQueue)
                   thread.start()
@@ -55,7 +57,7 @@ def runBackup(options):
             for job in jobs:
                 if(job.enabled):
                     if directorInstance.checkRemoteHost(job):
-                        if not options.dryrun:
+                        if not dryrun:
                             # Add to queue
                             workQueue.put(job)
             queueLock.release()            
@@ -71,7 +73,7 @@ def runBackup(options):
                 t.join()
             durationstats['backupenddatetime'] = int(time.time())
         
-            if not options.dryrun:
+            if not dryrun:
                 # Do housekeeping
                 durationstats['housekeepingstartdatetime'] = int(time.time())
                 for job in jobs:
@@ -85,13 +87,26 @@ def runBackup(options):
     except ProcessRunningException as m:
         logger().error(m)
         
+def checkRemoteHost(jobpath):
+    """ Check remote host and exit with exitcode 0 (success) or 1 (error) """
+    directorInstance = director()
+    jobs = directorInstance.getJobArray(jobpath)
+    checkRemoteHost = directorInstance.checkRemoteHost(jobs[0])
+    exit(not checkRemoteHost)
+        
 if __name__ == "__main__":
     options = setupCliArguments()
     config(options.mainconfig)
+    checkSingleHost = False
     
     # Welcome message
     if options.verbose:
         print "Starting AutoRsyncBackup"
+    
+    if options.job and options.dryrun:
+        checkSingleHost = True
+        options.verbose = True
+        config().debuglevel = 2
     
     # Set logpath
     logger(config().logfile)
@@ -99,9 +114,11 @@ if __name__ == "__main__":
     logger().setVerbose(options.verbose)
     for msg in config().debugmessages:
         logger().debug(msg)
-        
+
     if options.version:
         print getVersion()
         exit(0)
+    elif checkSingleHost:
+        checkRemoteHost(options.job)
     else:
-        runBackup(options)
+        runBackup(options.job, options.dryrun)
