9398672a819a8160853932c60bb15270c407b8af
Bas Couwenberg <bcouwenberg@deanone.nl>
2019-12-10 13:39:45 +0100

    flake8: E501 line too long (N > 79 characters)

--- src/models/job.py
+++ src/models/job.py
@@ -42,7 +42,8 @@ class job():
             with open(self.filepath, 'r') as stream:
                 jobconfig = yaml.load(stream)
         except Exception:
-            logger().error("Error while reading %s, skiping job" % self.filepath)
+            logger().error("Error while reading %s, skiping job"
+                           % self.filepath)
             self.enabled = False
             return False
 
@@ -50,7 +51,8 @@ class job():
             self.enabled = jobconfig['enabled']
         except Exception:
             self.enabled = True
-            logger().debug("%s: No enabled tag is set, using default value: True" % self.filepath)
+            logger().debug(("%s: No enabled tag is set,"
+                            " using default value: True") % self.filepath)
 
         try:
             self.hostname = jobconfig['hostname']
@@ -63,19 +65,22 @@ class job():
             self.ssh = jobconfig['ssh']
         except Exception:
             self.ssh = False
-            logger().debug("%s: No SSH jobconfig variable set." % self.filepath)
+            logger().debug("%s: No SSH jobconfig variable set."
+                           % self.filepath)
 
         try:
             self.ssh_sudo = jobconfig['ssh_sudo']
         except Exception:
             self.ssh_sudo = False
-            logger().debug("%s: No ssh_sudo jobconfig variable set." % self.filepath)
+            logger().debug("%s: No ssh_sudo jobconfig variable set."
+                           % self.filepath)
 
         try:
             self.sshusername = jobconfig['ssh_username']
         except Exception:
             if self.ssh:
-                logger().info("%s: No ssh_username is set, skipping job." % self.filepath)
+                logger().info("%s: No ssh_username is set, skipping job."
+                              % self.filepath)
                 self.enabled = False
                 return False
 
@@ -83,7 +88,8 @@ class job():
             if not self.ssh:
                 self.rsyncusername = jobconfig['rsync_username']
         except Exception:
-            logger().info("%s: No rsync_username is set, skipping job." % self.filepath)
+            logger().info("%s: No rsync_username is set, skipping job."
+                          % self.filepath)
             self.enabled = False
             return False
 
@@ -91,7 +97,8 @@ class job():
             if not self.ssh:
                 self.rsyncpassword = jobconfig['rsync_password']
         except Exception:
-            logger().info("%s: No password is set while not using SSH, skipping job." % self.filepath)
+            logger().info(("%s: No password is set while not using SSH,"
+                           " skipping job.") % self.filepath)
             self.enabled = False
             return False
 
@@ -99,7 +106,9 @@ class job():
             self.sshprivatekey = jobconfig['ssh_privatekey']
         except Exception:
             if self.ssh:
-                logger().error("%s: SSH is set, but no ssh_privatekey is configured, disabling backup" % self.filepath)
+                logger().error(("%s: SSH is set, but no ssh_privatekey is"
+                                " configured, disabling backup")
+                               % self.filepath)
                 self.enabled = False
                 return False
 
@@ -108,10 +117,12 @@ class job():
         except Exception:
             if self.ssh:
                 self.port = 22
-                logger().debug("%s: No rsync+ssh port is set, using default." % self.filepath)
+                logger().debug("%s: No rsync+ssh port is set, using default."
+                               % self.filepath)
             else:
                 self.port = 873
-                logger().debug("%s: No rsync port is set, using default." % self.filepath)
+                logger().debug("%s: No rsync port is set, using default."
+                               % self.filepath)
 
         try:
             if not self.ssh:
@@ -119,7 +130,8 @@ class job():
             else:
                 self.rsyncshare = ''
         except Exception:
-            logger().info("%s: No rsync_share is set, skipping job." % self.filepath)
+            logger().info("%s: No rsync_share is set, skipping job."
+                          % self.filepath)
             self.enabled = False
             return False
 
@@ -127,66 +139,78 @@ class job():
             self.backupdir = jobconfig['backupdir']
         except Exception:
             self.backupdir = config().backupdir
-            logger().debug("%s: No backupdir is set, using default" % self.filepath)
+            logger().debug("%s: No backupdir is set, using default"
+                           % self.filepath)
 
         try:
             self.speedlimitkb = int(jobconfig['speedlimitkb'])
         except Exception:
             self.speedlimitkb = config().speedlimitkb
-            logger().debug("%s: No or invalid speedlimitkb is set, using default" % self.filepath)
+            logger().debug(("%s: No or invalid speedlimitkb is set,"
+                            " using default") % self.filepath)
 
         try:
             self.dailyrotation = jobconfig['dailyrotation']
         except Exception:
             self.dailyrotation = config().dailyrotation
-            logger().debug("%s: No dailyrotation is set, using default" % self.filepath)
+            logger().debug("%s: No dailyrotation is set, using default"
+                           % self.filepath)
 
         try:
             self.weeklyrotation = jobconfig['weeklyrotation']
         except Exception:
             self.weeklyrotation = config().weeklyrotation
-            logger().debug("%s: No weeklyrotation is set, using default" % self.filepath)
+            logger().debug("%s: No weeklyrotation is set, using default"
+                           % self.filepath)
 
         try:
             self.monthlyrotation = jobconfig['monthlyrotation']
         except Exception:
             self.monthlyrotation = config().monthlyrotation
-            logger().debug("%s: No monthlyrotation is set, using default" % self.filepath)
+            logger().debug("%s: No monthlyrotation is set, using default"
+                           % self.filepath)
 
         try:
             self.weeklybackup = jobconfig['weeklybackup']
         except Exception:
             self.weeklybackup = config().weeklybackup
-            logger().debug("%s: No weeklybackup is set, using default" % self.filepath)
+            logger().debug("%s: No weeklybackup is set, using default"
+                           % self.filepath)
 
         try:
             self.monthlybackup = jobconfig['monthlybackup']
         except Exception:
             self.monthlybackup = config().monthlybackup
-            logger().debug("%s: No monthlybackup is set, using default" % self.filepath)
+            logger().debug("%s: No monthlybackup is set, using default"
+                           % self.filepath)
 
         try:
             self.hooks = jobconfig['hooks']
             if self.sshusername is None or self.sshprivatekey is None:
-                logger().error('%s: Missing ssh username or keyfile, hooks disabled' % self.filepath)
+                logger().error(('%s: Missing ssh username or keyfile,'
+                                ' hooks disabled') % self.filepath)
             else:
                 for hook in self.hooks:
                     try:
                         self.addhook(hook)
                     except KeyError as e:
-                        logger().error("%s: Error in hook definition: %s" % (self.filepath,  e))
+                        logger().error("%s: Error in hook definition: %s"
+                                       % (self.filepath,  e))
                         return False
         except Exception as e:
-            logger().info("%s: No hooks defined, skipping hooks (exception: %s)." % (self.filepath,  e))
+            logger().info(("%s: No hooks defined, skipping hooks"
+                           " (exception: %s).") % (self.filepath, e))
 
         try:
             self.include = jobconfig['include']
         except Exception:
             try:
                 self.include = jobconfig['fileset']
-                logger().warning("%s: fileset detected; consider using include." % self.filepath)
+                logger().warning(("%s: fileset detected;"
+                                  " consider using include.") % self.filepath)
             except Exception:
-                logger().info("%s: Neither include nor fileset are set, skipping job." % self.filepath)
+                logger().info(("%s: Neither include nor fileset are set,"
+                               " skipping job.") % self.filepath)
                 self.enabled = False
                 return False
 
@@ -194,7 +218,8 @@ class job():
             self.exclude = jobconfig['exclude']
         except Exception:
             self.exclude = config().exclude
-            logger().debug("%s: No exclude is set, using default" % self.exclude)
+            logger().debug("%s: No exclude is set, using default"
+                           % self.exclude)
 
     def addhook(self,  hook):
         if 'script' not in hook:
